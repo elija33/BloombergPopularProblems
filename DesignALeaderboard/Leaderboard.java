7709fe463846380f32b6b93723b3afc8e91ea590
Elija33 <amponsaeli@gmail.com>
2023-03-09 14:11:01 -0500

    Design A Leader Board

--- DesignALeaderboard/Leaderboard.java
+++ DesignALeaderboard/Leaderboard.java
@@ -1,19 +1,40 @@
 package DesignALeaderboard;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
 public class Leaderboard {
+    HashMap<Integer, Integer> scores;
+
+    public Leaderboard(){
+        scores = new HashMap<>();
+    }
 
     public void addScore(int playerId, int score){
-
+        scores.put(playerId, score + scores.getOrDefault(playerId, 0));
     }
 
     public int top(int k){
-        return -1;
+        List<Integer> topScores = new ArrayList<>(scores.values());
+        Collections.sort(topScores);
+        Collections.reverse(topScores);
+        System.out.println(topScores);
+
+        int total = 0;
+        for(int i = 0; i < k; i++){
+            total += topScores.get(i);
+        }
+        return total;
     }
 
     public void reset(int playerId){
-        
+        scores.remove(playerId);
     }
     public static void main(String[] args) {
-        
+        String name[] = {"Leaderboard","addScore","addScore","addScore","addScore","addScore","top","reset","reset","addScore","top"};
+        System.out.println();
     }
 }
